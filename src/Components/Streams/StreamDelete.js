bb25fea57cd0996bf18c60f7c23c7bb4d7d2d452
vatsalgp <vatsalgupta280@gmail.com>
2020-02-21 16:59:44 +0530

    Added Delete Functionality

--- src/Components/Streams/StreamDelete.js
+++ src/Components/Streams/StreamDelete.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+import Modal from "../Modal";
+import history from "../../history";
 import { fetchStream, deleteStream } from "../../Actions";
-import StreamForm from "./StreamForm";
 
 class StreamDelete extends React.Component {
 
@@ -9,8 +12,25 @@ class StreamDelete extends React.Component {
         this.props.fetchStream(this.props.match.params.id);
     }
 
-    onSubmit = () => {
-        this.props.deleteStream(this.props.match.params.id);
+    renderActions() {
+        return (
+            <div>
+                <button
+                    className="ui button negative"
+                    onClick={() => this.props.deleteStream(this.props.match.params.id)}//
+                >Delete</button>
+
+                <Link to="/" className="ui button">Cancel</Link>
+            </div>
+        );
+    }
+
+    renderContent() {
+        if (!this.props.stream) {
+            return "Are you sure you want to delete this stream ?";
+        } else {
+            return "Are you sure you want to delete the stream with title: " + this.props.stream.title + " ?";
+        }
     }
 
     render() {
@@ -18,16 +38,12 @@ class StreamDelete extends React.Component {
             return <div>Loading...</div>;
         } else {
             return (
-                <div>
-                    <h3>Delete a Stream</h3>
-                    <StreamForm
-                        onSubmit={this.onSubmit}
-                        initialValues={{
-                            title: this.props.stream.title,
-                            description: this.props.stream.description
-                        }}
-                    />
-                </div>
+                < Modal
+                    title="Delete Stream"
+                    content={this.renderContent()}
+                    actions={this.renderActions()}
+                    onDismiss={() => history.push("/")}
+                />
             );
         }
     }
